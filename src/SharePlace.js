5ddfba32f27f58dc705c31735e93717eb4457209
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-26 10:45:04 +0200

    Lesson 458

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,5 +1,6 @@
 import { Modal } from './UI/Modal';
 import {Map} from './UI/Map';
+import {getCoordsFromAddress} from './Utility/Location'
 
 class PlaceFinder {
   constructor() {
@@ -41,7 +42,23 @@ class PlaceFinder {
     });
   };
 
-  findAddressHandler() {};
+  async findAddressHandler(event) {
+    event.preventDefault();
+    const address = event.target.querySelector('input').value;
+    if(!address || address.trim().length === 0) {
+      alert('Invalid address. Please, try again!');
+      return;
+    }
+    const modal = new Modal ('loading-modal-content', 'Loading location please wait!');
+    // modal.show();
+    try {
+      const coordinates = await getCoordsFromAddress(address);
+      this.selectPlace(coordinates);
+    } catch(err) {
+      alert(err.message);
+    }
+    // modal.hide();
+  };
 }
 
 new PlaceFinder();
