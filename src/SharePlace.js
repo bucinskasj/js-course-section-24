0c06ddc881d22b3e130e23790c9b72c30f35f9aa
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-26 07:44:49 +0200

    Lesson 451

---
+++ src/SharePlace.js
@@ -0,0 +1,15 @@
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector('form');
+    const locateUserBtn = document.getElementById('locate-btn');
+
+    locateUserBtn.addEventListener('click', this.locateUserHandler);
+    addressForm.addEventListener('submit', this.findAddressHandler);
+  }
+
+  locateUserHandler() {
+    
+  };
+
+  findAddressHandler() {};
+}
