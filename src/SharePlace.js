1048107b7ec99e35bb1d0e6e2ab1755a1e1027b2
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-27 10:03:46 +0200

    Runs on Node.js and MongoDB now

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,103 +1,113 @@
-import { Modal } from './UI/Modal';
-import {Map} from './UI/Map';
-import {getCoordsFromAddress, getAddressFromCoords} from './Utility/Location'
+import { Modal } from "./UI/Modal";
+import { Map } from "./UI/Map";
+import { getCoordsFromAddress, getAddressFromCoords } from "./Utility/Location";
 
 class PlaceFinder {
   constructor() {
-    const addressForm = document.querySelector('form');
-    const locateUserBtn = document.getElementById('locate-btn');
-    this.shareBtn = document.getElementById('share-btn');
+    const addressForm = document.querySelector("form");
+    const locateUserBtn = document.getElementById("locate-btn");
+    this.shareBtn = document.getElementById("share-btn");
 
-    locateUserBtn.addEventListener('click', this.locateUserHandler.bind(this));
-    this.shareBtn.addEventListener('click', this.sharePlaceHandler);
-    addressForm.addEventListener('submit', this.findAddressHandler.bind(this));
+    locateUserBtn.addEventListener("click", this.locateUserHandler.bind(this));
+    this.shareBtn.addEventListener("click", this.sharePlaceHandler);
+    addressForm.addEventListener("submit", this.findAddressHandler.bind(this));
   }
 
   sharePlaceHandler() {
-    const sharedLinkInputElement = document.getElementById('share-link');
-    if(!navigator.clipboard) {
+    const sharedLinkInputElement = document.getElementById("share-link");
+    if (!navigator.clipboard) {
       sharedLinkInputElement.select();
       return;
     }
 
-    navigator.clipboard.writeText(sharedLinkInputElement.value)
-    .then(() => {
-      alert('Copied into clipboard')
-    })
-    .catch(() => {
-      console.log(err);
-      sharedLinkInputElement.select();
-    });
+    navigator.clipboard
+      .writeText(sharedLinkInputElement.value)
+      .then(() => {
+        alert("Copied into clipboard");
+      })
+      .catch(() => {
+        console.log(err);
+        sharedLinkInputElement.select();
+      });
   }
 
   selectPlace(coordinates, address) {
-    if(this.map) {
+    if (this.map) {
       this.map.render(coordinates);
     } else {
       this.map = new Map(coordinates);
     }
-    fetch('http://localhost:3000/add-location', {
-      method: 'POST',
+    fetch("http://localhost:3000/add-location", {
+      method: "POST",
       body: JSON.stringify({
         address: address,
         lat: coordinates.lat,
-        lng: coordinates.lng
+        lng: coordinates.lng,
       }),
       headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(response => {
-      return response.json();
+        "Content-Type": "application/json",
+      },
     })
-    .then(data => {
-      console.log(data);
-    });
-    this.shareBtn.disabled = false;
-    const sharedLinkInputElement = document.getElementById('share-link');
-    sharedLinkInputElement.value = `${location.origin}/my-place?address=${encodeURI(address)}&lat=${coordinates.lat}&lng=${coordinates.lng}`;
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const locationId = data.locId;
+        this.shareBtn.disabled = false;
+        const sharedLinkInputElement = document.getElementById("share-link");
+        sharedLinkInputElement.value = `${
+          location.origin
+        }/my-place?location=${locationId}`;
+      });
   }
 
   locateUserHandler() {
     if (!navigator.geolocation) {
-      alert(
-        'Location feature is not available in your browser'
-      );
+      alert("Location feature is not available in your browser");
       return;
-    };
-    const modal = new Modal('loading-modal-content', 'Loading location please wait!');
+    }
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading location please wait!"
+    );
     modal.show();
-    navigator.geolocation.getCurrentPosition( async successResult => {
-      const coordinates = {
-        lat: successResult.coords.latitude,
-        lng: successResult.coords.longitude
-      };
-      const address = await getAddressFromCoords(coordinates);
-      modal.hide();
-      this.selectPlace(coordinates, address);
-    }, 
-    error => {
-      modal.hide();
-      alert('Could not locate you unfortunately');
-    });
-  };
+    navigator.geolocation.getCurrentPosition(
+      async (successResult) => {
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
+        };
+        const address = await getAddressFromCoords(coordinates);
+        modal.hide();
+        this.selectPlace(coordinates, address);
+      },
+      (error) => {
+        modal.hide();
+        alert("Could not locate you unfortunately");
+      }
+    );
+  }
 
   async findAddressHandler(event) {
     event.preventDefault();
-    const address = event.target.querySelector('input').value;
-    if(!address || address.trim().length === 0) {
-      alert('Invalid address. Please, try again!');
+    const address = event.target.querySelector("input").value;
+    if (!address || address.trim().length === 0) {
+      alert("Invalid address. Please, try again!");
       return;
     }
-    const modal = new Modal ('loading-modal-content', 'Loading location please wait!');
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading location please wait!"
+    );
     // modal.show();
     try {
       const coordinates = await getCoordsFromAddress(address);
       this.selectPlace(coordinates, address);
-    } catch(err) {
+    } catch (err) {
       alert(err.message);
     }
     // modal.hide();
-  };
+  }
 }
 
 new PlaceFinder();
